e814be5d0871e23f789b4752c855257d3a0c7c3b
Meek Msaki <98189596+mmsaki@users.noreply.github.com>
2025-02-10 02:04:44 -0600

    ✨ feat: check for iOS, not displaying side panels

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -5,11 +5,12 @@ import CanvasLeft from "./canvas/canvas-left";
 import CanvasRight from "./canvas/canvas-right";
 
 export const Page: React.FC = () => {
+	const isIOS = /iPhone|iPad|iPod/i.test(navigator.userAgent);
 	return (
 		<div className="flex h-[calc(100vh-24px)]">
-			<CanvasLeft />
+			{!isIOS && <CanvasLeft />}
 			<Canvas />
-			<CanvasRight />
+			{!isIOS &&<CanvasRight />}
 		</div>
 	);
 };
