7c155f5c856fb737b5df1aaf19943cf5d09bafd7
Meek Msaki <98189596+mmsaki@users.noreply.github.com>
2025-02-10 01:50:41 -0600

    🦋 feat: activate storybook plugins

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,16 +1,18 @@
 import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: [
-    "@storybook/addon-onboarding",
-    "@storybook/addon-essentials",
-    "@chromatic-com/storybook",
-    "@storybook/addon-interactions",
-  ],
-  framework: {
-    name: "@storybook/react-vite",
-    options: {},
-  },
+	stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+	addons: [
+		"@storybook/addon-onboarding",
+		"@storybook/addon-essentials",
+		"@chromatic-com/storybook",
+		"@storybook/addon-interactions",
+		"@storybook/addon-designs",
+		"@storybook/addon-a11y",
+	],
+	framework: {
+		name: "@storybook/react-vite",
+		options: {},
+	},
 };
 export default config;
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,14 +1,16 @@
 import type { Preview } from "@storybook/react";
+import "../src/App.css";
 
+/** @type { import('@storybook/react').Preview } */
 const preview: Preview = {
-  parameters: {
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i,
-      },
-    },
-  },
+	parameters: {
+		controls: {
+			matchers: {
+				color: /(background|color)$/i,
+				date: /Date$/i,
+			},
+		},
+	},
 };
 
 export default preview;
